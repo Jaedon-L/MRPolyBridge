77950358b27fee141a4893a14a4b359fe333a5fb
Jaime Diaz <Jdiaz651@outlook.com>
2025-06-17 21:48:02 -0400

    Button Script revision
    Added button color change option, as well as minor code adjustment.

--- MRPolyBridge/Assets/TextChanger.cs
+++ MRPolyBridge/Assets/TextChanger.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
 using TMPro;
+using Oculus.Interaction; // Required for MaterialPropertyBlockEditor
+
 public class TextChanger : MonoBehaviour
 {
-    [SerializeField] TextMeshPro supportMode;
+    [Header("Display Text")]
+    [SerializeField] private TextMeshPro supportMode;
+    [SerializeField] private string OnText;
+    [SerializeField] private string OffText;
+
+    [Header("Optional Visual Feedback for Button")]
+    [SerializeField] private MaterialPropertyBlockEditor roundedBoxEditor;
+    [SerializeField] private string colorPropertyName = "_Color"; // or "_BaseColor" depending on shader
+    [SerializeField] private Color onColor;
+    [SerializeField] private Color offColor;
+
     private bool supportModeState;
-    [SerializeField] string OnText; 
-    [SerializeField] string OffText; 
+
     public void OnToggleSupport()
     {
         supportModeState = !supportModeState;
-        if (supportModeState)
+        supportMode.text = supportModeState ? OnText : OffText;
+
+        if (roundedBoxEditor != null && !string.IsNullOrEmpty(colorPropertyName))
         {
-            supportMode.text = OnText;
-        }
-        else
-        {
-            supportMode.text = OffText;
+            roundedBoxEditor.ColorProperties = new()
+            {
+                new MaterialPropertyColor
+                {
+                    name = colorPropertyName,
+                    value = supportModeState ? onColor : offColor
+                }
+            };
+            roundedBoxEditor.UpdateMaterialPropertyBlock();
         }
     }
-
 }
